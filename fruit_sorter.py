ff7ca5242a88ee0774fb58adb6c93dfd229256e3
noamb19-meet <noamb19@meet.mit.edu>
2017-07-26 12:23:56 +0300

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,11 @@
+new_fruit=input('what fruit am I storing?')
+if new_fruit=='apples':
+    print('bin 1')
+elif new_fruit=='oranges':
+    print('bin 2')
+
+elif new_fruit== 'olives':
+    print('bin 3')
+else:
+    print('error!I do not recognise this fruit!')
+    
---
+++ turtle_party.py
@@ -0,0 +1,8 @@
+strawberrises=60
+is_weekend=False
+if is_weekend and strawberrises>39:
+    print('fun!')
+elif not is_weekend and strawberrises>39 and strawberrises<61:
+    print('fun!')
+else:
+    print('not fun')
